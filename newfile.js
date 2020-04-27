ffaed4af5e16efb1ea8b7b82090b9d73cf875151
DustinDEV2more <Dustin@Dustin-DM.de>
2020-04-27 14:26:45 +0200

    Add some new News Pages

--- newfile.js
+++ newfile.js
@@ -2,13 +2,13 @@ var NEWS = require("./models/NEWS")
 
 async function doit() {
     var newnews = new NEWS({
-    id: 0004,
-    date: "27.03.2020",
-    pic: "images/Adidas.png",
-    t: "Adidas zahlt nicht mehr",
-    prevtxt: "Adidas will anscheinend über die Corona Zeit Mietzahlungen aussetzten so Jan Runau. Man soll eng mit den Vermietern im Gespräch sein. Das Problem der Läden besteht allerdings nicht nur in Deutschland. Auch in der USA gibt es Läden die geschlossen bleiben. Die einzige Option ist der Online Handel von Adidas. ",
-    txt: `Adidas will anscheinend über die Corona Zeit Mietzahlungen aussetzten so Jan Runau. Man soll eng mit den Vermietern im Gespräch sein. Das Problem der Läden besteht allerdings nicht nur in Deutschland. Auch in der USA gibt es Läden die geschlossen bleiben. Die einzige Option ist der Online Handel von Adidas. Dieser bringt allerdings nur 15 % des Umsatzes in die Taschen von Adidas. Adidas steht hier allerdings nicht alleine da. Auch andere Ketten wie H&M und Deichmann wollen Mietzahlungen aussetzten. Die Vermutung das viele weiteren Ketten folgen liegt nahe. Es wurden demnach allerdings auch ein paar neue gesetzte eingerichtet. So darf ein Vermieter jetzt nach einer Mietaussetzung erst nach 2 Jahren kündigen! Dieses Recht zählt sowohl für Gewerbe als auch für privat Personen und ist daran gebunden das die Verzögerung der Zahlung durch Corona entstanden ist. `,
-    q: "https://www.tagesschau.de/inland/adidas-gewerbemiete-bundesrat-101.html"
+    id: 8,
+    date: "28.03.2020",
+    pic: "images/Wer das ließt sollte für mich arbeiten.png",
+    t: "Mieter müssen ihre Miete zahlen",
+    prevtxt: "Viele Unternehmen wollen aktuell keine Miete zahlen da sie sowieso keine Einnahmen generieren. Unter anderem Adidas, H&M und Deichmann hatten am Freitag entsprechende Schritte ab April verkündet und wollen ihre Mietzahlungen verstunden. Die Bundesregierung hält das für unanständig und unsolidarisch.",
+    txt: `Viele Unternehmen wollen aktuell keine Miete zahlen da sie sowieso keine Einnahmen generieren. Unter anderem Adidas, H&M und Deichmann hatten am Freitag entsprechende Schritte ab April verkündet und wollen ihre Mietzahlungen verstunden. Die Bundesregierung hält das für unanständig und unsolidarisch. Mieter haben weiterhin ihre Zahlungspflicht zu erfüllen und diese geht nur flöten, wenn man in Zahlungsengpässen kommt. Die Bundesregierung hatte als Teil der Corona-Hilfsgesetze entsprechende Anpassungen beschlossen. Diese sehen vor, dass das Kündigungsrecht bei ausbleibender Mietzahlung für die Dauer von zwei Jahren ausgesetzt wird. Miete, die in den nächsten drei Monaten nicht gezahlt wird, muss erst bis zum 30.06.2022 nachgeleistet sein.`,
+    q: "https://www.tagesschau.de/wirtschaft/reaktion-gewerbemiete-corona-101.html"
 
     }).save().then(console.log("News saved"))
 }
