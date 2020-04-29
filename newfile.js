f798bca2fb61a9b91a1afca27fd8d3f674bab1a9
DustinDEV2more <Dustin@Dustin-DM.de>
2020-04-29 17:33:28 +0200

    Add more news pictures

--- newfile.js
+++ newfile.js
@@ -2,13 +2,13 @@ var NEWS = require("./models/NEWS")
 
 async function doit() {
     var newnews = new NEWS({
-    id: 8,
-    date: "28.03.2020",
-    pic: "images/Wer das ließt sollte für mich arbeiten.png",
-    t: "Mieter müssen ihre Miete zahlen",
-    prevtxt: "Viele Unternehmen wollen aktuell keine Miete zahlen da sie sowieso keine Einnahmen generieren. Unter anderem Adidas, H&M und Deichmann hatten am Freitag entsprechende Schritte ab April verkündet und wollen ihre Mietzahlungen verstunden. Die Bundesregierung hält das für unanständig und unsolidarisch.",
-    txt: `Viele Unternehmen wollen aktuell keine Miete zahlen da sie sowieso keine Einnahmen generieren. Unter anderem Adidas, H&M und Deichmann hatten am Freitag entsprechende Schritte ab April verkündet und wollen ihre Mietzahlungen verstunden. Die Bundesregierung hält das für unanständig und unsolidarisch. Mieter haben weiterhin ihre Zahlungspflicht zu erfüllen und diese geht nur flöten, wenn man in Zahlungsengpässen kommt. Die Bundesregierung hatte als Teil der Corona-Hilfsgesetze entsprechende Anpassungen beschlossen. Diese sehen vor, dass das Kündigungsrecht bei ausbleibender Mietzahlung für die Dauer von zwei Jahren ausgesetzt wird. Miete, die in den nächsten drei Monaten nicht gezahlt wird, muss erst bis zum 30.06.2022 nachgeleistet sein.`,
-    q: "https://www.tagesschau.de/wirtschaft/reaktion-gewerbemiete-corona-101.html"
+    id: 10,
+    date: "29.03.2020",
+    pic: "images/INDEN CORONA.png",
+    t: "Welche Folgen das Corona auf Inden hat",
+    prevtxt: `Durch das Coronavirus ist auch in der indischen Stadt Delhi eine Ausgangssperre verhängt worden. Nun hängen 10.000 Arbeiter verzweifelt in der Hauptstadt fest und versuchen verzweifelt zurück in ihre Heimatdörfer zu kommen. Nach der Ausgangssperre, die von der Regierung kommt wurden viele Menschen, die durch den Arbeitsstopp nicht mehr Arbeiten konnten Obdachlos und Arbeitslos.`,
+    txt: `Durch das Coronavirus ist auch in der indischen Stadt Delhi eine Ausgangssperre verhängt worden. Nun hängen 10.000 Arbeiter verzweifelt in der Hauptstadt fest und versuchen verzweifelt zurück in ihre Heimatdörfer zu kommen. Nach der Ausgangssperre, die von der Regierung kommt wurden viele Menschen, die durch den Arbeitsstopp nicht mehr Arbeiten konnten Obdachlos und Arbeitslos. Wobei zweiteres sich nach der Kreise wieder lösen sollte. Ein Riesiges Gedrängel besteht besonders an allen Bahnhöfen der Statt. Versuche der Polizisten um Ordnung in die Menge zu bekommen sind vergebens. Alle wollen so schnell wie möglich raus. Die Regierung von Uttar Pradesh hat jetzt angekündigt Verkehrsmittel bereits zu stellen, um gestrandete Menschen zurück in ihre Dörfer zu bekommen. (Kommentar des Autors: Wäre der Lockdown der Stadt nicht dann total unnötig?) Auch medizinische Hilfe wurde angekündigt.`,
+    q: "https://www.tagesschau.de/ausland/indien-corona-103.html"
 
     }).save().then(console.log("News saved"))
 }
